b06f28dae9ed90d0d5bf959206fb61adc6c53287
felipe <contatofelipevidalmendes@gmail.com>
2022-07-01 10:46:56 -0300

    testando js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//retornar no console todas as imagens
+const imagens_site = document.querySelectorAll('img');
+console.log(imagens_site);
+
+//retorne as iagens que começe com imagem
+const imagens_por_nome = document.querySelectorAll('img[src^="img/imagem2.jpg"]');
+console.log(imagens_por_nome);
+
+// selecione todos os links internos onde o hrfe comece como #
+const link = document.querySelectorAll('[href^="#"]');
+
+
