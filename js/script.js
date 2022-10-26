d33be89b0e69b30d6714eb7c51c4334d66b1d901
felipe <contatofelipevidalmendes@gmail.com>
2022-10-26 13:18:33 -0300

    otimizado

--- js/script.js
+++ js/script.js
@@ -7,21 +7,26 @@ const tab_conteudo = document.querySelectorAll('.js-tabconteudo section');
 tab_conteudo[0].classList.add('ativo');
 
 
-// adiciona class de acordo com um numero passado
-function ativar_tab (Numero_tab_ativar){
+//verificando se os itens existem
+if(tab_menu.length && tab_conteudo.length){
 
-    //remove todos as class ativos antes de adicionar a class
-    tab_conteudo.forEach( (section_percorrida) =>{
-        section_percorrida.classList.remove('ativo');
-    } );
+    // adiciona class de acordo com um numero passado
+    function ativar_tab (Numero_tab_ativar){
 
-    //pega o conteudo em array e adiciona a class ativo
-    tab_conteudo[Numero_tab_ativar].classList.add('ativo');
-}
+        //remove todos as class ativos antes de adicionar a class
+        tab_conteudo.forEach( (section_percorrida) =>{
+            section_percorrida.classList.remove('ativo');
+        } );
 
-// evento que captura o numero de onde foi clicado e adiciona na função de ativar tab
-tab_menu.forEach( (item_menu_percorrido, index_item_clicado) => {
-    item_menu_percorrido.addEventListener('click', ()=> {
-        ativar_tab(index_item_clicado);
+        //pega o conteudo em array e adiciona a class ativo
+        tab_conteudo[Numero_tab_ativar].classList.add('ativo');
+    }
+
+    // evento que captura o numero de onde foi clicado e adiciona na função de ativar tab
+    tab_menu.forEach( (item_menu_percorrido, index_item_clicado) => {
+        item_menu_percorrido.addEventListener('click', ()=> {
+            ativar_tab(index_item_clicado);
+        });
     });
-});
+
+}
