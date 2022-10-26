fa7d6e09ad171731652876cd28b015f4862d24f3
felipe <contatofelipevidalmendes@gmail.com>
2022-10-26 12:09:06 -0300

    captura de index ok

--- js/script.js
+++ js/script.js
@@ -8,6 +8,18 @@ const tab_conteudo = document.querySelectorAll('.js-tabconteudo section');
 // adiciona class de acordo com um numero passado
 function ativar_tab (Numero_tab_ativar){
 
+    //remove todos as class ativos antes de adicionar a class
+    tab_conteudo.forEach( (section_percorrida) =>{
+        section_percorrida.classList.remove('ativo');
+    } );
+
     //pega o conteudo em array e adiciona a class ativo
     tab_conteudo[Numero_tab_ativar].classList.add('ativo');
 }
+
+// evento que captura o numero de onde foi clicado e adiciona na função de ativar tab
+tab_menu.forEach( (item_menu_percorrido, index_item_clicado) => {
+    item_menu_percorrido.addEventListener('click', ()=> {
+        ativar_tab(index_item_clicado);
+    });
+});
