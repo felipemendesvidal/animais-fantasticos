9f63dd54024af59027b485e776b20374a488dcdc
felipe <contatofelipevidalmendes@gmail.com>
2022-11-07 13:27:24 -0300

    isolando funcoes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
-//selecionando os elementos da li
-const tab_menu = document.querySelectorAll('.js-tabmenu li');
 
-//selecionando conteudo 
-const tab_conteudo = document.querySelectorAll('.js-tabconteudo section');
-// sempre colocando o primeiro item ativo
-tab_conteudo[0].classList.add('ativo');
+//isolando o tab nav
+function iniciar_navegacao_tab(){
+
+    //selecionando os elementos da li
+    const tab_menu = document.querySelectorAll('.js-tabmenu li');
+
+    //selecionando conteudo 
+    const tab_conteudo = document.querySelectorAll('.js-tabconteudo section');
+    // sempre colocando o primeiro item ativo
+    tab_conteudo[0].classList.add('ativo');
 
 
-//verificando se os itens existem
-if(tab_menu.length && tab_conteudo.length){
+    //verificando se os itens existem
+    if(tab_menu.length && tab_conteudo.length){
 
-    // adiciona class de acordo com um numero passado
-    function ativar_tab (Numero_tab_ativar){
+        // adiciona class de acordo com um numero passado
+        function ativar_tab (Numero_tab_ativar){
 
-        //remove todos as class ativos antes de adicionar a class
-        tab_conteudo.forEach( (section_percorrida) =>{
-            section_percorrida.classList.remove('ativo');
-        } );
+            //remove todos as class ativos antes de adicionar a class
+            tab_conteudo.forEach( (section_percorrida) =>{
+                section_percorrida.classList.remove('ativo');
+            } );
 
-        //pega o conteudo em array e adiciona a class ativo
-        tab_conteudo[Numero_tab_ativar].classList.add('ativo');
-    }
+            //pega o conteudo em array e adiciona a class ativo
+            tab_conteudo[Numero_tab_ativar].classList.add('ativo');
+        }
 
-    // evento que captura o numero de onde foi clicado e adiciona na função de ativar tab
-    tab_menu.forEach( (item_menu_percorrido, index_item_clicado) => {
-        item_menu_percorrido.addEventListener('click', ()=> {
-            ativar_tab(index_item_clicado);
+        // evento que captura o numero de onde foi clicado e adiciona na função de ativar tab
+        tab_menu.forEach( (item_menu_percorrido, index_item_clicado) => {
+            item_menu_percorrido.addEventListener('click', ()=> {
+                ativar_tab(index_item_clicado);
+            });
         });
-    });
 
+    }
 }
+
+
+
+
+//area de ativação das funcoes
+iniciar_navegacao_tab();
